67d41e82cc51e55a29250e1c30b84424b092b41b
Björn Platte <lonoxX96@gmail.com>
2024-04-13 17:45:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Discord Remove all commands
+
+This project is a Discord bot that removes all commands.
+
+
+
+### ⭐ This repository if you like this project!
+
+## Installation
+
+Follow these steps to install the project:
+
+1. Clone the repository:
+```sh
+git clone https://github.com/LonoxX/DiscordBot-Deletingcommands.git
+```
+
+2. Navigate into the project's directory:
+```sh
+cd DiscordBot-Deletingcommands
+```
+
+3. Install the dependencies:
+```sh
+npm install
+```
+
+4. Copy the `example_config.json` file and rename it to `config.json`. Update the configuration details accordingly:
+```sh
+cp example_config.json config.json
+```
+
+5. Start the project:
+```sh
+npm start
+```
+
+
+# Support
+
+> Website: [Panda-Network.de](https://panda-network.de) \
+> Support [Discord Server](https://discord.gg/z8ScRvf)
---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { Client, GatewayIntentBits, Partials, Routes } = require("discord.js");
+const { clientId, guildId, token } = require("./config.json");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildVoiceStates],
+  partials: [Partials.Channel, Partials.Message, Partials.GuildMember],
+});
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+async function removeSlashCommands(type) {
+  switch (type) {
+    case "guild":
+      rest
+        .put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+        .then(() => {
+          console.log("Successfully deleted all guild commands.");
+        })
+        .catch(console.error);
+      break;
+    case "global":
+      rest
+        .put(Routes.applicationCommands(clientId), { body: [] })
+        .then(() => {
+          console.log("Successfully deleted all global commands.");
+        })
+        .catch(console.error);
+      break;
+    default:
+      console.log("Invalid type");
+  }
+}
+
+client.once("ready", async () => {
+  await removeSlashCommands("global");
+  client.destroy();
+});
+
+client.login(token);
